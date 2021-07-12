fb279fa59204dede49f25adf91a251b0406588ed
Mark Daryl Grimaldo <grimaldomarkdaryl@gmail.com>
2021-07-12 20:17:52 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+  var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+  var randomImageOne = "images/dice" + randomNumber1 + ".png";
+
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageOne);
+
+  var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+  var randomImageTwo = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageTwo);
+
+
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Mommy wins"
+}else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Daddy wins"
+}else {
+  document.querySelector("h1").innerHTML = "It's a Draw!"
+}
